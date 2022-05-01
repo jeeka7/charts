34ae0247c9853eab851446bf0158898a33ab7fae
Dean Hunter <jeeka7@gmail.com>
2022-05-02 03:43:48 +0530

    Update darksoul3.py

--- darksoul3.py
+++ darksoul3.py
@@ -4,5 +4,5 @@ import streamlit as st
 st.header('Weapon used in dark souls 3 are')
 st.subheader('was the tutorial helpful')
 
-weapons=pd.read_csv("https://raw.githubusercontent.com/jeeka7/charts/main/DS3weapon.csv")
+weapons=pd.read_csv('https://raw.githubusercontent.com/jeeka7/charts/main/DS3weapon.csv')
 print(weapons)
