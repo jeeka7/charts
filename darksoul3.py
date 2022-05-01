d2294517b728dafb1886c368a0d6d11e86df511c
Dean Hunter <jeeka7@gmail.com>
2022-05-02 03:45:10 +0530

    Update darksoul3.py

--- darksoul3.py
+++ darksoul3.py
@@ -5,4 +5,4 @@ st.header('Weapon used in dark souls 3 are')
 st.subheader('was the tutorial helpful')
 
 weapons=pd.read_csv('https://raw.githubusercontent.com/jeeka7/charts/main/DS3weapon.csv')
-print(weapons)
+st.write(weapons)
