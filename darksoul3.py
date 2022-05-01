e8470749bdfbf1468ada36d0d88ed2847865e926
Dean Hunter <jeeka7@gmail.com>
2022-05-02 04:31:59 +0530

    Update darksoul3.py

--- darksoul3.py
+++ darksoul3.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 
 st.header('Weapon used in dark souls 3 are')
 st.subheader('was the tutorial helpful')
 
 weapons=pd.read_csv('https://raw.githubusercontent.com/jeeka7/charts/main/DS3weapon.csv')
 st.write(weapons)
+pie_chart = px.pie(weapons,
+                title='Total No. of Participants',
+                values='Participants',
+                names='Departments')
+
+st.plotly_chart(pie_chart)
